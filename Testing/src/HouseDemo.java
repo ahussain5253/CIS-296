9d576bf7fb3277f66b423d7c1cd271d90d764f55
Ammar Hussain <ammarh786@gmail.com>
2022-10-28 10:50:06 -0400

    a

---
+++ Testing/src/HouseDemo.java
@@ -0,0 +1,20 @@
+public class HouseDemo {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+
+            if ((i % 2) != 0) {
+
+                System.out.println(i + " ");
+
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
